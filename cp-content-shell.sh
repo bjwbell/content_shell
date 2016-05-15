d926c0d2e4cfe758e79fb8446b6e8fd69929c639
JW Bell <bjwbell@gmail.com>
2016-05-15 16:05:40 -0700

    Copy and gzip content_shell executable

---
+++ cp-content-shell.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+rm -rf ~/cp-content-shell
+mkdir ~/cp-content-shell
+cd ~/src/out/Default
+cp icudtl.dat ~/cp-content-shell
+cp *.so ~/cp-contentn-shell
+cp content_shell ~/cp-content-shell
+cd ~
+tar -zvcf cp-content-shell.tar.gz cp-content-shell
+
