9ec32a678cd8d72396921af14f43347af43d6f82
JW Bell <bjwbell@gmail.com>
2016-05-26 23:28:44 -0700

    Fix directory name typo in cp-content-shell.sh

--- cp-content-shell.sh
+++ cp-content-shell.sh
@@ -3,7 +3,7 @@ rm -rf ~/cp-content-shell
 mkdir ~/cp-content-shell
 cd ~/src/out/Default
 cp icudtl.dat ~/cp-content-shell
-cp *.so ~/cp-contentn-shell
+cp *.so ~/cp-content-shell
 cp content_shell ~/cp-content-shell
 cd ~
 tar -zvcf cp-content-shell.tar.gz cp-content-shell
