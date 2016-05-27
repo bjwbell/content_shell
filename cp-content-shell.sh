8bef0bca88ca7ab18b010b3a43adc6a6d63f19fb
JW Bell <bjwbell@gmail.com>
2016-05-26 23:37:36 -0700

    cp-content-shell.sh - Add *.bin files

--- cp-content-shell.sh
+++ cp-content-shell.sh
@@ -3,6 +3,7 @@ rm -rf ~/cp-content-shell
 mkdir ~/cp-content-shell
 cd ~/src/out/Default
 cp icudtl.dat ~/cp-content-shell
+cp *.bin ~/cp-content-shell
 cp *.so ~/cp-content-shell
 cp content_shell ~/cp-content-shell
 cd ~
