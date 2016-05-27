63ffd4e5ca7da0814d6bcaffc5736d95c5a92942
JW Bell <bjwbell@gmail.com>
2016-05-26 23:54:51 -0700

    Rename cp-content-shell directory to content-shell

--- cp-content-shell.sh
+++ cp-content-shell.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-rm -rf ~/cp-content-shell
-mkdir ~/cp-content-shell
+rm -rf ~/content-shell
+mkdir ~/content-shell
 cd ~/src/out/Default
-cp icudtl.dat ~/cp-content-shell
-cp *.bin ~/cp-content-shell
-cp *.pak ~/cp-content-shell
-cp *.so ~/cp-content-shell
-cp content_shell ~/cp-content-shell
+cp icudtl.dat ~/content-shell
+cp *.bin ~/content-shell
+cp *.pak ~/content-shell
+cp *.so ~/content-shell
+cp content_shell ~/content-shell
 cd ~
-tar -zvcf cp-content-shell.tar.gz cp-content-shell
+tar -zvcf content-shell.tar.gz content-shell
 
