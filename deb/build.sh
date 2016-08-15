d89615e27ba8ebafec33f9b9acf4f64148d73fb3
JW Bell <bjwbell@gmail.com>
2016-08-15 16:15:54 -0400

    Switch lintian commandline opts order

--- deb/build.sh
+++ deb/build.sh
@@ -4,4 +4,4 @@ sudo apt-get install bzr-builddeb
 sudo apt-get install dh-buildinfo ninja-build libexif-dev chrpath yasm libopus-dev libspeex-dev libwebp-dev libsrtp-dev libjsoncpp-dev libevent-dev xdg-utils
 bzr branch lp:~chromium-team/chromium-browser/trusty-working
 cd trusty-working
-bzr builddeb --builder debuild -- --lintian-opts --no-lintian -uc -us
+bzr builddeb --builder debuild -- -uc -us --lintian-opts --no-lintian
