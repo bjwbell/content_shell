7c63367c280ad92df3b1f04bbdb72f3503082b33
JW Bell <bjwbell@gmail.com>
2016-08-15 08:41:38 -0400

    Add deb/ with packaging instructions

---
+++ deb/README.md
@@ -0,0 +1,4 @@
+# Building deb package
+Ubuntu deb packaging repo - bzr branch lp:~chromium-team/chromium-browser/trusty-working
+To build deb pacakge, execute `build.sh`.
+
---
+++ deb/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo apt-get install bzr
+sudo apt-get install bzr-builddeb
+sudo apt-get install dh-buildinfo ninja-build libexif-dev chrpath yasm libopus-dev libspeex-dev libwebp-dev libsrtp-dev libjsoncpp-dev libevent-dev xdg-utils
+bzr branch lp:~chromium-team/chromium-browser/trusty-working
+cd trusty-working
+export ORIG_VERSION="51.0.2704.106"
+bzr builddeb
