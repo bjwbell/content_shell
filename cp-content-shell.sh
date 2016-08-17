1e22a392f818a5b309342500b5a7f81fa063ff3b
JW Bell <bjwbell@gmail.com>
2016-08-17 12:46:18 -0400

    Add needed files for chrome binary

--- cp-content-shell.sh
+++ cp-content-shell.sh
@@ -8,5 +8,9 @@ cp *.bin ~/content-shell
 cp *.pak ~/content-shell
 cp *.so ~/content-shell
 cp content_shell ~/content-shell
+cp chrome ~/content-shell
+cp -r locales ~/content-shell
+cp -r resources ~/content-shell
+cp -r ui ~/content-shell
 cd ~
 tar -zvcf content-shell.tar.gz content-shell
