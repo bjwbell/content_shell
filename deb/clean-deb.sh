ae339c777e8246a903eb8386b39d0b1ac35c3b2c
JW Bell <bjwbell@gmail.com>
2016-08-18 00:27:36 -0400

    Remove additional deb package files

--- deb/clean-deb.sh
+++ deb/clean-deb.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 rm chromium-*
+rm ironframe_1.1.1.1-0ubuntu1.dsc
+rm ironframe_1.1.1.1-0ubuntu1.tar.gz
+rm ironframe_1.1.1.1-0ubuntu1_amd64.build
+rm ironframe_1.1.1.1-0ubuntu1_amd64.changes
 rm -rf chrome-deb
